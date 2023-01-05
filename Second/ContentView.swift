9937658fafb94288b1fa7d3c0853fb5cf76d0fb2
tluu9798 <trungluu9798@gmail.com>
2023-01-05 15:33:09 +0700

    Initial Commit

---
+++ Second/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Second
+//
+//  Created by Trung Luu on 5/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Second/SecondApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondApp.swift
+//  Second
+//
+//  Created by Trung Luu on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct SecondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
